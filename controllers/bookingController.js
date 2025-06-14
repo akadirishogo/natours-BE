0e1a6943ad6ad141be22696809b46c985683fe83
ShogoMark <trigmark87@gmail.com>
2025-06-14 10:09:49 +0100

    fixed session error

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -46,7 +46,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.display_items[0].price_data.unit_amount / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -66,7 +66,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`)
   }
 
-  if (event.type === 'checkout.session.complete')
+  if (event.type === 'checkout.session.completed')
      createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
